4014000c9c7df9656772aaf7b8a9885e8dccfbb5
Michael O Brien <obrien.michael@outlook.ie>
2023-05-25 01:31:36 +0100

    Added more blank pages and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AboutComponent } from './modules/pages/about/about.component';
 import { HomeComponent } from './modules/pages/home/home.component';
+import { AboutComponent } from './modules/pages/about/about.component';
+import { ContactComponent } from './modules/pages/contact/contact.component';
+import { ProjectsComponent } from './modules/pages/projects/projects.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'projects', component: ProjectsComponent}
 ];
 
 @NgModule({
