7306b014723c1c88de8b78adba8e7c174d23054e
Michael O Brien <obrien.michael@outlook.ie>
2023-05-25 01:00:41 +0100

    Added about-me component and fixed the app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutMeComponent } from './home/pages/aboutme/about-me/about-me.component';
+import { LandingComponent } from './home/pages/landing/landing/landing.component';
+
+const routes: Routes = [
+  {path: '', component: LandingComponent},
+  {path: 'about-me', component: AboutMeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
